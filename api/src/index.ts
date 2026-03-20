c5c55f3570f04932a87ab4ce7a9415e259264505
FranklinMonro <fbg2468@gmail.com>
2026-03-20 04:58:56 +0000

    create web

--- api/src/index.ts
+++ api/src/index.ts
@@ -8,6 +8,13 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Hello World! This is a TypeScript backend.');
 });
 
+app.post('/convert/:example', (req: Request, res: Response) => {
+  const  { example } = req.params as { example: string };
+  const arrayString = example.split('');
+  const sortedArray = arrayString.sort();
+  res.status(200).json({ word: sortedArray})
+});
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
