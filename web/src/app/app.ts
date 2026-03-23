d57b9736ae6e6fd1cdefeaf278c9b0845dfe10fb
FranklinMonro <fbg2468@gmail.com>
2026-03-23 05:31:59 +0000

    add card for input

--- web/src/app/app.ts
+++ web/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 
+import {MatCardModule} from '@angular/material/card';
 import {MatToolbarModule} from '@angular/material/toolbar';
 @Component({
   selector: 'app-root',
-  imports: [MatToolbarModule],
+  imports: [MatToolbarModule, MatCardModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
