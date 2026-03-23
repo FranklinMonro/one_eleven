a65ac7c00e966b674025d6cdca0ab9f97bf45283
FranklinMonro <fbg2468@gmail.com>
2026-03-23 13:43:31 +0000

    add reactive forms

--- web/src/app/app.ts
+++ web/src/app/app.ts
@@ -1,15 +1,28 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 
 import {MatCardModule} from '@angular/material/card';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
+import { FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
-  imports: [MatToolbarModule, MatCardModule, MatFormFieldModule, MatInputModule],
+  imports: [MatToolbarModule, MatCardModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('web');
+  wordForm!: FormGroup;
+
+  ngOnInit(): void {
+    this.wordForm = new FormGroup({
+      word: new FormControl<string>(''),
+      email: new FormControl<string>('', [Validators.email]),
+    });
+  }
+
+  onSubmit() {
+    console.log(this.wordForm.value);
+  }
 }
