956746bf43b779ada22853a6e06baebd162e14d0
FranklinMonro <fbg2468@gmail.com>
2026-03-23 13:38:49 +0000

    add form fields

--- web/src/app/app.ts
+++ web/src/app/app.ts
@@ -2,9 +2,11 @@ import { Component, signal } from '@angular/core';
 
 import {MatCardModule} from '@angular/material/card';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 @Component({
   selector: 'app-root',
-  imports: [MatToolbarModule, MatCardModule],
+  imports: [MatToolbarModule, MatCardModule, MatFormFieldModule, MatInputModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
